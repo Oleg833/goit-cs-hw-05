230e9a79d74c3707a86a29b3edfd8ef73466a3bf
Oleg833 <oleg.kovtun447@gmail.com>
2024-03-14 10:31:22 +0200

    upd task_2

--- task_2.py
+++ task_2.py
@@ -6,47 +6,16 @@ from functools import reduce
 from collections import defaultdict
 
 
-# Виконання MapReduce в одній функції
-# def map_reduce(text):
-#     # Крок Map: розбиття тексту на слова та створення пар (слово, 1)
-#     words = text.split()
-#     mapped_values = [(word, 1) for word in words]
-
-#     # Крок Shuffle: групування значень за ключами
-#     shuffled = defaultdict(list)
-#     for key, value in mapped_values:
-#         shuffled[key].append(value)
-
-#     # Крок Reduce: обрахунок суми значень для кожного ключа
-#     reduced = {key: sum(values) for key, values in shuffled.items()}
-
-#     return reduced
-
-
 def map_reduce(text):
     words = text.split()
-    # Крок Map:  створення пар (слово, 1)
     mapped_words = map(lambda word: (word, 1), words)
     sorted_mapped_words = sorted(mapped_words)
-    # reduced_words = reduce(
-    #     lambda acc, word_val: acc.update({word_val[0]: acc.get(word_val[0], 0) + 1}) or acc,
-    #     sorted_words,
-    #     {},
-    # )
-    reduced_words = {}
-    for word_val in sorted_mapped_words:
-        if word_val[0] in reduced_words:
-            reduced_words[word_val[0]] += 1
-        else:
-            reduced_words[word_val[0]] = 1
-    # Вивести перші 10 елементів словника
-    i = 0
-    for word in reduced_words:
-        i += 1
-        if i < 10:
-            print(word, reduced_words[word])
-        else:
-            break
+    reduced_words = reduce(
+        lambda acc, word_val: acc.update({word_val[0]: acc.get(word_val[0], 0) + 1})
+        or acc,
+        sorted_mapped_words,
+        {},
+    )
 
     return reduced_words
 
@@ -55,7 +24,7 @@ def visualize_top_words(word_counts, top_n=20):
     top_words = dict(Counter(word_counts).most_common(top_n))
     plt.bar(top_words.keys(), top_words.values())
     plt.xlabel("Слова")
-    plt.ylabel("Частота")
+    plt.ylabel("Частота використання")
     plt.title("Топ-слова за частотою використання")
     plt.xticks(rotation=45)
     plt.show()
