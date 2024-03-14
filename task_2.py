42d168a6e8a2819e773d269ab1d0af53320adcae
Oleg833 <oleg.kovtun447@gmail.com>
2024-03-15 01:33:33 +0200

    add ThreadPoolExecutor

--- task_2.py
+++ task_2.py
@@ -1,46 +1,57 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
 from collections import Counter
 import re
 import matplotlib.pyplot as plt
-from functools import reduce
-from collections import defaultdict
 
 
-def map_reduce(text):
-    words = text.split()
-    mapped_words = map(lambda word: (word, 1), words)
-    sorted_mapped_words = sorted(mapped_words)
-    reduced_words = reduce(
-        lambda acc, word_val: acc.update({word_val[0]: acc.get(word_val[0], 0) + 1})
-        or acc,
-        sorted_mapped_words,
-        {},
-    )
+def fetch_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
 
-    return reduced_words
+
+def map_word_frequency(document):
+    words = document.lower().split()
+    return Counter(words)
+
+
+def reduce_word_frequency(counters):
+    total_counts = Counter()
+    for word_counts in counters:
+        total_counts.update(word_counts)
+    return total_counts
 
 
 def visualize_top_words(word_counts, top_n=20):
-    top_words = dict(Counter(word_counts).most_common(top_n))
-    plt.bar(top_words.keys(), top_words.values())
+    top_words = word_counts.most_common(top_n)
+    print(top_words)
+    words, counts = zip(*top_words)
+
+    plt.figure(figsize=(10, 8))
+    plt.bar(words, counts)
     plt.xlabel("Слова")
     plt.ylabel("Частота використання")
-    plt.title("Топ-слова за частотою використання")
     plt.xticks(rotation=45)
+    plt.title("Топ-слова з найвищою частотою використання")
     plt.show()
-
-
-def fetch_text(url):
-    response = requests.get(url)
-    return response.text
-
-
-def main(url):
-    text = fetch_text(url)
-    word_counts = map_reduce(text)
-    visualize_top_words(word_counts)
 
 
 if __name__ == "__main__":
     url = "https://www.gutenberg.org/files/11/11-0.txt"  # Alice in Wonderland by Lewis Carroll
-    main(url)
+    text = fetch_content(url)
+
+    if text:
+        parts = [text[i : i + 1000] for i in range(0, len(text), 1000)]
+
+        with ThreadPoolExecutor() as executor:
+            counters = list(executor.map(map_word_frequency, parts))
+
+        total_counts = reduce_word_frequency(counters)
+
+        visualize_top_words(total_counts)
